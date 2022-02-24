b8794be22ab0b39789d870fcfc78494c746b6949
JoseCima <josegcima@gmail.com>
2022-02-24 13:55:26 -0500

    Crear un swiper de tarjetas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,55 @@
+
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas - JoseCima'),
+        backgroundColor: Colors.pink,
+        actions: <Widget>[
+          IconButton(
+             onPressed: (){},
+             icon:Icon(Icons.search),
+             ),
+
+        ],
+      ),
       body:
-       SafeArea(
-         child:  Text('Hola Mundo Jose Cima'),
-         )
-      
+      Container(
+        child: Column(
+          children: <Widget>[
+            _swiperWidget()
+          ],
+        ),
+      )
     );
   }
+
+
+
+  Widget _swiperWidget(){
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+          itemBuilder: (BuildContext context,int index){
+            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill);
+          },
+          itemCount: 3,
+          //pagination: new SwiperPagination(),//puntos
+          //control: new SwiperControl(),//deslizar
+        ),
+    );
+  }
+
 }
