559291ec22d00bb9fc39bd33d16640a5d35039db
JoseCima <josegcima@gmail.com>
2022-02-24 15:56:52 -0500

    Peliculas provider

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider{
+  String _apikey = 'a33d872fb1fb624d3d061784191f4b33';
+  String _url = 'api.themoviedb.org';
+  String _languaje = 'es-ES';
+
+
+ Future <List<Pelicula>> getEnCines() async{
+     
+   final url = Uri.https(_url, '3/movie/now_playing', {
+     
+     'api_key'    : _apikey,
+     'languaje'   : _languaje
+
+     
+     
+
+
+   });
+
+   final resp = await http.get(url);
+   final decodedData = json.decode(resp.body);
+
+   final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+
+   return peliculas.items;
+
+  }
+}
